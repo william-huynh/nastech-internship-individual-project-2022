eb54f4f702b59b35afe2c648307c2b7665834551
William Huynh <hnbaolam@gmail.com>
2022-06-16 18:22:03 +0700

    Add project files.

---
+++ ClotheShop.CustomerSite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ClotheShop.CustomerSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
