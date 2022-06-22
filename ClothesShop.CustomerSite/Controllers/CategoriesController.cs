733d45664bfec783d34117f648dc66df5872951d
William Huynh <hnbaolam@gmail.com>
2022-06-22 22:29:47 +0700

    feature (ClothesShop.CustomerSite): Add GET single category method and Single Page. Finish GET single category using Id API

--- ClothesShop.CustomerSite/Controllers/CategoriesController.cs
+++ ClothesShop.CustomerSite/Controllers/CategoriesController.cs
@@ -2,6 +2,7 @@
 using ClothesShop.SharedVMs.Categories;
 using Newtonsoft.Json;
 using System.Net.Http.Formatting;
+using System.Text;
 
 namespace ClothesShop.CustomerSite.Controllers
 {
@@ -10,17 +11,22 @@ namespace ClothesShop.CustomerSite.Controllers
         Uri baseAddress = new Uri("https://localhost:7167/api");
         HttpClient httpClient;
 
-        CategoriesReadDto _oCategory = new CategoriesReadDto();
-        List<CategoriesReadDto> _oCategories = new List<CategoriesReadDto>();
+        // Read category/categories
+        CategoriesReadDto _oCategoryRead = new CategoriesReadDto();
+        List<CategoriesReadDto> _oCategoriesRead = new List<CategoriesReadDto>();
+
+        // Create category
+        CategoriesCreateDto _oCategoryCreate = new CategoriesCreateDto();
 
         public CategoriesController()
         {
             httpClient = new HttpClient();
         }
 
+        // GET (all) categories
         public async Task<IActionResult> Index()
         {
-            _oCategories = new List<CategoriesReadDto>();
+            _oCategoriesRead = new List<CategoriesReadDto>();
             using (httpClient)
             {
                 using (var response = await httpClient.GetAsync(baseAddress + "/Categories"))
@@ -28,11 +34,61 @@ namespace ClothesShop.CustomerSite.Controllers
                     string apiResponse = await response.Content.ReadAsStringAsync();
 
                     #pragma warning disable CS8601 // Possible null reference assignment.
-                    _oCategories = JsonConvert.DeserializeObject<List<CategoriesReadDto>>(apiResponse);
+                    _oCategoriesRead = JsonConvert.DeserializeObject<List<CategoriesReadDto>>(apiResponse);
                     #pragma warning restore CS8601 // Possible null reference assignment.
                 }
             }
-            return View(_oCategories);
+            return View(_oCategoriesRead);
+        }
+
+        // GET (single) category
+        public ViewResult Single() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Single(int id)
+        {
+            _oCategoryRead = new CategoriesReadDto();
+            using (httpClient)
+            {
+                using (var response = await httpClient.GetAsync(baseAddress + "/Categories/" + id))
+                {
+                    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+
+                        #pragma warning disable CS8601 // Possible null reference assignment.
+                        _oCategoryRead = JsonConvert.DeserializeObject<CategoriesReadDto>(apiResponse);
+                        #pragma warning restore CS8601 // Possible null reference assignment.
+                    }
+                    else
+                    {
+                        ViewBag.StatusCode = response.StatusCode;
+                    }
+                }
+            }
+            return View(_oCategoryRead);
+        }
+
+        // POST category
+        public ViewResult Create() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CategoriesCreateDto _categoryCreate)
+        {
+            _oCategoryRead = new CategoriesReadDto();
+            using (httpClient)
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(_categoryCreate), Encoding.UTF8, "application/json");
+                using (var response = await httpClient.PostAsync(baseAddress + "/Categories", content))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    
+                    #pragma warning disable CS8601 // Possible null reference assignment.
+                    _oCategoryRead = JsonConvert.DeserializeObject<CategoriesReadDto>(apiResponse);
+                    #pragma warning restore CS8601 // Possible null reference assignment.
+                }
+            }
+            return View(_oCategoryRead);
         }
     }
 }
