64c0bbc846c8b6855e65de2490c7b9e860ea2322
William Huynh <hnbaolam@gmail.com>
2022-06-28 22:14:37 +0700

    feature (ClothesShop.CustomerSite): initialize branch and create LoginController and Login view

---
+++ ClothesShop.CustomerSite/Controllers/LoginController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ClothesShop.CustomerSite.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index() => View();
+
+        [HttpPost]
+        public IActionResult Index(string username, string password)
+        {
+            return View();
+        }
+    }
+}
--- ClothesShop.CustomerSite/Program.cs
+++ ClothesShop.CustomerSite/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Categories}/{action=Index}/{id?}");
+    pattern: "{controller=Login}/{action=Index}");
 
 app.Run();
