3bab4fec9bcd796e76f3e4dc7d55aca6af8b7f28
William Huynh <hnbaolam@gmail.com>
2022-07-01 13:45:46 +0700

    improve (ClothesShop.CustomerSite): Add Services folder to store interface services. Using Refit to simplify REST APIs

---
+++ ClothesShop.CustomerSite/Services/ICategoriesService.cs
@@ -0,0 +1,28 @@
+﻿using ClothesShop.SharedVMs;
+using Refit;
+
+namespace ClothesShop.CustomerSite.Services
+{
+    public interface ICategoriesService
+    {
+        // Get all categories
+        [Get("/Categories")]
+        Task<List<CategoryDto>> GetCategories();
+
+        // Get category by Id
+        [Get("/Categories/{id}")]
+        Task<CategoryDto> GetCategory(int id);
+
+        // Add category
+        [Post("/Categories")]
+        Task CreateCategory([Body] CategoryDto category);
+
+        // Update category
+        [Put("/Categories/{id}")]
+        Task UpdateCategory(int id, [Body] CategoryDto category);
+
+        // Delete category
+        [Delete("/Categories/{id}")]
+        Task DeleteCategory(int id);
+    }
+}
