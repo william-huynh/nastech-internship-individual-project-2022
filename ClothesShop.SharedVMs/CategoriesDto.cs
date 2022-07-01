d2dfa4e8e01021f1e1184b36b9040761d621c0ed
William Huynh <hnbaolam@gmail.com>
2022-07-01 12:06:27 +0700

    feature (ClothesShop.SharedVMs): Merge CategoriesDto into CategoryDto and change Profile for different mapping use

--- ClothesShop.SharedVMs/CategoriesDto.cs
+++ ClothesShop.SharedVMs/CategoriesDto.cs
@@ -1,10 +1,11 @@
-﻿namespace ClothesShop.SharedVMs.Categories
+﻿namespace ClothesShop.SharedVMs
 {
-    public class CategoriesUpdateDto
+    public class CategoryDto
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
+        public bool IsDeleted { get; set; }
         public int ProductQuantity { get; set; }
     }
 }
