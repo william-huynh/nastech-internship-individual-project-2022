5f8b8019b6cd2ae7d6113cb623744337e5fdef1f
William Huynh <hnbaolam@gmail.com>
2022-07-04 17:02:04 +0700

    feature (ClothesShop.CustomerSite): Initialize branch. Add container class to homepage. Create Clothes category and initialize products index page

---
+++ ClothesShop.CustomerSite/Controllers/ClothesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ClothesShop.CustomerSite.Controllers
+{
+    public class ClothesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
